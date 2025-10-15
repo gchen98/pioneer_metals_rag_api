13650d548f3ee49343c5e16420235bd60f39d051
Gary Chen <gchen98@gmail.com>
2025-10-14 18:04:20 -0700

    Updated credential sample and examples

---
+++ test_process_po.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json"  -d @samples/example_json.txt http://localhost:8000/process-purchase-order
---
+++ test_upload_pdf.sh
@@ -0,0 +1,1 @@
+curl -X POST -F "file=@samples/PACKING_LIST_9.8.pdf" http://localhost:8000/process-pdf
